c1c2729cc7d28dd723f140e7888c278bb085084b
artursvaitilavics <artyvt@gmail.com>
2020-06-13 13:45:47 +0300

    Arrays: Task001 - Finish

--- src/Arrays/Task001.java
+++ src/Arrays/Task001.java
@@ -2,5 +2,19 @@ package Arrays;
 
 public class Task001 {
     //Write a program to find the index of an array element.
+    String[] myArray = {"a", "b", "c", "d", "e", "f"};
+
+    public void getIndex(String element) {
+        int index = -1;
+        for (int i = 0; i < myArray.length; i++) {
+            if (element.equals(myArray[i])) {
+                index = i;
+            }
+        }
+        System.out.println("Find index of \""
+                + element +
+                "\". Result "
+                + index);
+    }
 
 }
--- src/ExerciseApp.java
+++ src/ExerciseApp.java
@@ -1,3 +1,4 @@
+import Arrays.Task001;
 import LoopTasks.*;
 
 import java.util.Scanner;
@@ -5,24 +6,15 @@ import java.util.Scanner;
 public class ExerciseApp {
 
     public static void main(String[] args) {
-        //////////////////////////XYZ Inputs/////////////////////////////////////////////////////
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.println("Please eneter X: ");
-//        int x = scanner.nextInt();
-//        System.out.println("Please eneter Y: ");
-//        int y = scanner.nextInt();
-//        System.out.println("Please eneter Z: ");
-//        int z = scanner.nextInt();
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Please maximum denominator: ");
-        int maxDenominator = scanner.nextInt();
+        System.out.println("Please enter letter: ");
+        String userInput = scanner.nextLine();
 ////////////////////////////////////////////////////////////////////////////////////////////////
-        //TASKS//
 
-        Task12 task = new Task12();
-        task.forLoop(maxDenominator);
-        task.whileLoop(maxDenominator);
-        task.doWhileLoop(maxDenominator);
+        //TASKS Group 2 - Arrays//
+        Task001 task001 = new Task001();
+        task001.getIndex(userInput);
+
     }
 }
