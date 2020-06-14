4dff1476fa235d7917f093e45e2d1a6eb72bfac1
artursvaitilavics <artyvt@gmail.com>
2020-06-14 13:39:16 +0300

    Arrays stop for now.
    Try Game tasks

--- src/Arrays/Task005.java
+++ src/Arrays/Task005.java
@@ -1,5 +1,7 @@
 package Arrays;
 
+import java.util.Arrays;
+
 public class Task005 {
     //5. Write a method that will keep only unique Strings from String array.
     //    ```
@@ -8,38 +10,51 @@ public class Task005 {
     //   ... // strings contains ["e", "b", "c", "a", "d"]
     //    ```
 
+//    public String[] removeDuplicates(String[] inputArray) {
+//        String tempString = inputArray[0];
+//        String repeatded = "";
+//        String outputString = tempString;
+//        int countUnique = 0;
+//
+//
+//        for (int i = 0; i < inputArray.length; i++) {
+//            tempString = inputArray[i];
+//            for (int j = 0; j < inputArray.length; j++) {
+//                if (!tempString.equals(inputArray[j])) {
+//                    if (!outputString.contains(tempString)) {
+//                        outputString = outputString + inputArray[i];
+//                        countUnique++;
+//                    }
+//                }
+//            }
+//        }
+//    }
+
     public String[] removeDuplicates(String[] inputArray) {
-        String tempString = inputArray[0];
-        String repeatded = "";
-        String outputString = tempString;
+        int unique = 0;
+        String tempElement = "";
+
+        String[] array = new String[inputArray.length];
+//        array[0] = inputArray[0];
+//        tempElement = inputArray[0];
 
         for (int i = 0; i < inputArray.length; i++) {
-            tempString = inputArray[i];
-            for (int j = 0; j < inputArray.length; j++) {
-                if (!tempString.equals(inputArray[j])) {
-                    if (!outputString.contains(tempString)) {
-                        outputString = outputString + inputArray[i];
-                    }
-                }
-            }
+
         }
-        return arrayFromString(outputString);
+        System.out.println(unique);
+        return array;
     }
 
-
-    private String[] arrayFromString(String string){
+    private String[] arrayFromString(String string) {
         String[] outputStringArray = new String[string.length()];
         for (int i = 0; i < string.length(); i++) {
             outputStringArray[i] = String.valueOf(string.charAt(i));
         }
-        return  outputStringArray;
+        return outputStringArray;
     }
 
     public void printArray(String[] arrayToPrint) {
-        for (int i = 0; i < arrayToPrint.length; i++) {
-            System.out.print(" " + arrayToPrint[i]);
-        }
-        System.out.println();
+        System.out.println(Arrays.toString(arrayToPrint));
     }
 
 }
--- src/ExerciseApp.java
+++ src/ExerciseApp.java
@@ -5,6 +5,10 @@ public class ExerciseApp {
 
     public static void main(String[] args) {
         String[] array = {"e", "b", "b", "a", "e", "c", "a", "e", "d"};
+        String[] arrayWords = {"kartupelis", "kartupelis",
+                "kotlete", "merce",
+                "kartupelis", "gurki",
+                "merce", "merce"};
 
 
         //Task 05
@@ -12,9 +16,9 @@ public class ExerciseApp {
         Task005 task005 = new Task005();
 
         System.out.println("String array: ");
-        task005.printArray(array);
+        task005.printArray(arrayWords);
         System.out.println();
-        String[] array2 = task005.removeDuplicates(array);
+        String[] array2 = task005.removeDuplicates(arrayWords);
         System.out.println("String array contains: ");
         task005.printArray(array2);
 
