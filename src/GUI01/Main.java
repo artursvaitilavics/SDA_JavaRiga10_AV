6da884b437652d05ce57db7f2c4128db1038b2e2
artursvaitilavics <artyvt@gmail.com>
2020-07-03 23:27:11 +0300

    lesson 51 completed

--- src/GUI01/Main.java
+++ src/GUI01/Main.java
@@ -2,14 +2,14 @@ package GUI01;
 
 import javax.swing.*;
 
-public class Main {
+class Main {
 
     public static void main(String[] args) {
-        String string1 = JOptionPane.showInputDialog("Enter number One");
-        String string2 = JOptionPane.showInputDialog("Enter second number: ");
+        Window01 window01 = new Window01();
 
-        JOptionPane.showMessageDialog(null,
-                string1, "Arturs", JOptionPane.PLAIN_MESSAGE);
+        window01.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window01.setSize(300, 200);
+        window01.setVisible(true);
     }
 
 }
--- src/GUI01/Window01.java
+++ src/GUI01/Window01.java
@@ -3,11 +3,11 @@ package GUI01;
 import java.awt.FlowLayout;
 import javax.swing.*;
 
-public class Window01 extends JFrame{
+public class Window01 extends JFrame {
 
     private JLabel label1;
 
-    public Window01(){
+    public Window01() {
         super("Window01");
         setLayout(new FlowLayout());
 
