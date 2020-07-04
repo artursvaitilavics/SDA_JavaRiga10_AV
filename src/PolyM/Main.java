57efd1d84eb2e2b6fcb8ba35427c32719376efa2
artursvaitilavics <artyvt@gmail.com>
2020-07-04 07:55:15 +0300

    lesson 59 completed

--- src/PolyM/Main.java
+++ src/PolyM/Main.java
@@ -3,12 +3,10 @@ package PolyM;
 public class Main {
     public static void main(String[] args) {
 
-    Kamaz kamaz = new Kamaz();
-    Car car1 = new Car();
-    Car car2 = new Zaz();
+    ZazList zazList = new ZazList();
+    Zaz zaz = new Zaz();
 
-    kamaz.transport(car1);
-    kamaz.transport(car2);
+    zazList.add(zaz);
 
     }
 }
---
+++ src/PolyM/ZazList.java
@@ -0,0 +1,14 @@
+package PolyM;
+
+public class ZazList {
+    private Zaz[] theList = new Zaz[5];
+    private int i = 0;
+
+    public void add(Zaz _zaz) {
+        if (i < theList.length) {
+            theList[i] = _zaz;
+            System.out.println("Dog added at index " + i);
+            i++;
+        }
+    }
+}
