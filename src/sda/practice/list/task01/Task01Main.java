323a0b47e4170439cb4c54c49b1cc009c8edbdec
artursvaitilavics <artyvt@gmail.com>
2020-07-11 12:53:33 +0300

    List Task 01 - update

--- src/sda/practice/list/task01/Task01Main.java
+++ src/sda/practice/list/task01/Task01Main.java
@@ -9,14 +9,11 @@ package sda.practice.list.task01;
 //d)d)* View only purchases whose next product on the list starts with „m” ( e.g . eggs ,
 //if milk was next on the list) (edited)
 
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Task01Main {
     public static void main(String[] args) {
-        List<String> purchaseList = new LinkedList<>();  //Linked list, because will need to see next in list
+        List<String> purchaseList = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
         String product = "";
         String operation = "";
@@ -27,36 +24,37 @@ public class Task01Main {
                 case "add":
                     System.out.println("What to buy");
                     product = scanner.nextLine();
-                    purchaseList.add(product);
-                    printList(purchaseList);
+                    if (!purchaseList.contains(product)) {
+                        purchaseList.add(product);
+                    } else {
+                        System.out.println("Already in the list");
+                    }
+                    System.out.println(purchaseList.toString());
                     break;
                 case "remove":
                     System.out.println("Which product remove: ");
                     product = scanner.nextLine();
                     purchaseList.remove(product);
-                    printList(purchaseList);
+                    System.out.println(purchaseList.toString());
                 case "display":
                     System.out.println("Display product starting with letter: ");
                     String firstLetter = scanner.nextLine();
                     for (String item :
                             purchaseList) {
-                        String firstLetInList = String.valueOf(item.charAt(0));
-                        if (firstLetter.equals(firstLetInList)) {
+                        if (firstLetter.equals(item.startsWith(firstLetter))) {
                             System.out.println(item);
                         }
                     }
                 case "display next":
-                    Iterator<String> iterator = purchaseList.iterator();
                     System.out.println("Display product before product with first letter: ");
                     firstLetter = scanner.nextLine();
 
-                    for (String item :
-                            purchaseList) {
-                        if (!firstLetter.equals(String.valueOf(iterator.next().charAt(0)))){
-                            System.out.println(item);
+                    for (int i = 0; i < purchaseList.size(); i++) {
+                        if (purchaseList.get(i + 1).startsWith(firstLetter)) {
+                            System.out.println(purchaseList.get(i));
                         }
                     }
-                break;
+                    break;
                 default:
                     break;
             }
